1d8e1437081a1dbe3477a42622bdc827b9aeff75
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2024-08-04 15:31:25 -0500

    add cypress

---
+++ cypress.config.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  video: false,
+});
