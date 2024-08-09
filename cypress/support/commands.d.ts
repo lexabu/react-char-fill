2af875c0b780484c4eecbc1a3f977ecf8514f466
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2024-08-08 23:06:57 -0500

    update config

---
+++ cypress/support/commands.d.ts
@@ -0,0 +1,11 @@
+import { MountOptions, MountReturn } from 'cypress/react18';
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount(
+        component: React.ReactNode,
+        options?: MountOptions,
+      ): Chainable<MountReturn>;
+    }
+  }
+}
