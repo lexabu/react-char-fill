c01b52b45d6455b77b7efdd35b633577260550c4
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2024-08-09 18:32:16 -0500

    update for demo

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   root: './src/demo',
   plugins: [react()],
+  build: {
+    outDir: '../../docs', // Output the demo build to the docs directory
+    rollupOptions: {
+      input: path.resolve(__dirname, 'src/demo/index.html'),
+    },
+  },
   server: {
     open: true,
   },
