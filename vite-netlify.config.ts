39b046197b8ef1c9e212624a102ca05ad68c70f6
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2024-08-10 09:17:55 -0500

    remove base path from netlify

--- vite-netlify.config.ts
+++ vite-netlify.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   root: './src/demo',
-  base: '/react-char-fill/',
   plugins: [react()],
   build: {
     outDir: '../../docs-netlify',
