ffab4991cbce4317865507cb3500ac063c5fb798
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2024-08-09 20:29:47 -0500

    move sourcemaps to inline

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'path';
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
+    sourcemap: 'inline',
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'ReactCharFill',
