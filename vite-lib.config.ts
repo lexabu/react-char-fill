d7ce40752e99a0e85e5c50a39b9aa4e8e7844626
Alex Abushanab <38299309+lexabu@users.noreply.github.com>
2024-08-09 20:16:17 -0500

    generate types

---
+++ vite-lib.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+    lib: {
+      entry: resolve(__dirname, 'src/lib/index.ts'),
+      name: 'ReactCharFill',
+      fileName: format => `react-char-fill.${format}.js`,
+      formats: ['es', 'umd'],
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    outDir: 'dist',
+  },
+});
